e598e45c524a3ae4af8ad36b0266d836ed6ac4bd
Axis, the high wizard <asguilift@hotmail.com>
2023-08-28 22:23:27 -0300

    Initial Commit

---
+++ scripts/blackhole.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+async function main() {
+	const [signer] = await ethers.getSigners();
+	const Factory = await ethers.deployContract("Blackhole");
+	const Blackhole = await Factory.waitForDeployment();
+	const address = await Blackhole.getAddress();
+
+	console.log("\nBlackhole deployed to:", address);
+
+	const balance0x = await ethers.provider.getBalance(ethers.ZeroAddress);
+
+	console.log("\nBalance of:");
+	console.log("Zero Address: ", ethers.formatEther(balance0x));
+	console.log("Signer Address: ", ethers.formatEther(signer.address));
+
+	const balanceBlackhole = await ethers.provider.getBalance(address);
+	console.log("Balance of Blackhole: ", ethers.formatEther(balanceBlackhole));
+
+	console.log("---------end---------");
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {});
