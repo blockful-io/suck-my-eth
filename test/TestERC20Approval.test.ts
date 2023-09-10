6fed777f9df511b5c45ca228c5f0d25480449bc9
Axis, the high wizard <asguilift@hotmail.com>
2023-09-10 06:47:22 -0300

    add more permit tests

--- test/TestERC20Approval.test.ts
+++ test/TestERC20Approval.test.ts
@@ -18,43 +18,88 @@ describe("TestERC20", function () {
 		expect(await ERC20.symbol()).to.equal("BLACK");
 		expect(await ERC20.decimals()).to.equal(18);
 
-		await ERC20.mint(owner.address, 1000);
 		await ERC20.mint(userA.address, 1000);
-		await ERC20.mint(userB.address, 1000);
 	});
 
-	it("should approve 1000 tokens to userA", async () => {
-		await ERC20.approve(userA.address, 1000);
-		expect(await ERC20.allowance(owner.address, userA.address)).to.equal(1000);
+	it("should approve 1000 tokens to userA as userB", async () => {
+		expect(await ERC20.connect(userB).approve(userA.address, 1000))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 1000);
 	});
 
-	it("should approve to 0 tokens to userA", async () => {
-		await ERC20.approve(userA.address, 0);
-		expect(await ERC20.allowance(owner.address, userA.address)).to.equal(0);
+	it("should approve to 0 tokens to userA as userB", async () => {
+		expect(await ERC20.connect(userB).approve(userA.address, 0))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 0);
 	});
 
-	it("should approve to Max Uint256 tokens to userA", async () => {
-		await ERC20.approve(userA.address, ethers.constants.MaxUint256);
-		expect(await ERC20.allowance(owner.address, userA.address)).to.equal(
-			ethers.constants.MaxUint256
+	it("should approve to Max Uint256 tokens to userA as userB", async () => {
+		expect(
+			await ERC20.connect(userB).approve(
+				userA.address,
+				ethers.constants.MaxUint256
+			)
+		)
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, ethers.constants.MaxUint256);
+	});
+
+	it("Should consume allowance when transferring from userA to userB as userB", async () => {
+		await ERC20.mint(userA.address, 1000);
+
+		expect(await ERC20.connect(userA).approve(userB.address, 1000))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 1000);
+
+		await expect(
+			ERC20.connect(userB).transferFrom(userA.address, userB.address, 1000)
+		)
+			.to.emit(ERC20, "Transfer")
+			.withArgs(userA.address, userB.address, 1000);
+
+		expect(await ERC20.allowance(userA.address, userB.address)).to.be.equal(0);
+	});
+
+	it("Should not consume allowance at maxUint256 when transferring from userA to userB as userB", async () => {
+		await ERC20.mint(userA.address, 1000);
+
+		expect(await ERC20.connect(userA).approve(userB.address, 1000))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 1000);
+
+		await expect(
+			ERC20.connect(userB).transferFrom(userA.address, userB.address, 1000)
+		)
+			.to.emit(ERC20, "Transfer")
+			.withArgs(userA.address, userB.address, 1000);
+
+		expect(await ERC20.allowance(userA.address, userB.address)).to.be.equal(
+			ethers.constants.AddressZero
 		);
 	});
 
-	it("should fail to increase userA allowance when at Max Uint256", async () => {
-		await ERC20.approve(userA.address, ethers.constants.MaxUint256);
-		await expect(ERC20.increaseAllowance(userA.address, 1000)).to.be.rejected;
+	it("should fail to increase userA allowance when at Max Uint256 as userB", async () => {
+		await ERC20.connect(userB).approve(
+			userA.address,
+			ethers.constants.MaxUint256
+		);
+		await expect(ERC20.connect(userB).increaseAllowance(userA.address, 1000)).to
+			.be.rejected;
 	});
 
-	it("Should be able to increase allowance of 1000 tokens to userA", async () => {
-		await ERC20.approve(userA.address, 0);
-		await ERC20.increaseAllowance(userA.address, 1000);
-		expect(await ERC20.allowance(owner.address, userA.address)).to.equal(1000);
+	it("Should be able to increase allowance of 1000 tokens to userA as userB", async () => {
+		await ERC20.connect(userB).approve(userA.address, 0);
+		expect(await ERC20.connect(userB).increaseAllowance(userA.address, 1000))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 1000);
 	});
 
-	it("Should be able to decrease allowance of 1000 tokens to userA", async () => {
-		await ERC20.approve(userA.address, 0);
-		await ERC20.approve(userA.address, 1000);
-		await ERC20.decreaseAllowance(userA.address, 1000);
+	it("Should be able to decrease allowance of 1000 tokens to userA as userB", async () => {
+		await ERC20.connect(userB).approve(userA.address, 0);
+		await ERC20.connect(userB).approve(userA.address, 1000);
+		expect(await ERC20.connect(userB).decreaseAllowance(userA.address, 1000))
+			.to.emit(ERC20, "Approval")
+			.withArgs(userB.address, userA.address, 1000);
 		expect(await ERC20.allowance(owner.address, userA.address)).to.equal(0);
 	});
 
@@ -65,11 +110,11 @@ describe("TestERC20", function () {
 		).to.be.revertedWithCustomError(ERC20, "ERC20FailedDecreaseAllowance");
 	});
 
-	it("Should be able to permit userA to move tokens of owner", async () => {
-		const _owner = owner.address;
+	it("Should be able to permit userA to move tokens of userB", async () => {
+		const _owner = userB.address;
 		const spender = userA.address;
 		const value = 1000;
-		const nonce = await ERC20.nonces(owner.address);
+		const nonce = await ERC20.nonces(_owner);
 		const deadline = ethers.constants.MaxUint256;
 
 		const domain = {
@@ -97,7 +142,7 @@ describe("TestERC20", function () {
 			deadline: deadline,
 		};
 
-		const signature = await owner._signTypedData(domain, types, permit);
+		const signature = await userB._signTypedData(domain, types, permit);
 		const sig = ethers.utils.splitSignature(signature);
 
 		await ERC20.permit(_owner, spender, value, deadline, sig.v, sig.r, sig.s);
@@ -106,7 +151,7 @@ describe("TestERC20", function () {
 	});
 
 	it("Should not be able to use a permit a second time", async () => {
-		const _owner = owner.address;
+		const _owner = userB.address;
 		const spender = userA.address;
 		const value = 1000;
 		const nonce = await ERC20.nonces(_owner);
@@ -137,7 +182,7 @@ describe("TestERC20", function () {
 			deadline: deadline,
 		};
 
-		const signature = await owner._signTypedData(domain, types, permit);
+		const signature = await userB._signTypedData(domain, types, permit);
 		const sig = ethers.utils.splitSignature(signature);
 
 		await expect(
@@ -145,8 +190,83 @@ describe("TestERC20", function () {
 		).to.be.revertedWithCustomError(ERC20, "ERC2612InvalidSigner");
 	});
 
-	it("Should not be able to use an expired permit", async () => {});
-	it("Should not be able to use a permit with wrong signer", async () => {});
-	it("Should consume allowance when used", async () => {});
-	it("Should not consume allowance when set as max uint256", async () => {});
+	it("Should not be able to use an expired permit", async () => {
+		const _owner = userB.address;
+		const spender = userA.address;
+		const value = 1000;
+		const nonce = await ERC20.nonces(_owner);
+		const deadline = (await ethers.provider.getBlock("latest")).timestamp - 1;
+
+		const domain = {
+			name: "Blackhole",
+			version: "1",
+			chainId: (await ethers.provider.getNetwork()).chainId,
+			verifyingContract: ERC20.address,
+		};
+
+		const types = {
+			Permit: [
+				{ name: "owner", type: "address" },
+				{ name: "spender", type: "address" },
+				{ name: "value", type: "uint256" },
+				{ name: "nonce", type: "uint256" },
+				{ name: "deadline", type: "uint256" },
+			],
+		};
+
+		const permit = {
+			owner: _owner,
+			spender: spender,
+			value: value,
+			nonce: nonce.sub(1),
+			deadline: deadline,
+		};
+
+		const signature = await userB._signTypedData(domain, types, permit);
+		const sig = ethers.utils.splitSignature(signature);
+
+		await expect(
+			ERC20.permit(_owner, spender, value, deadline, sig.v, sig.r, sig.s)
+		).to.be.revertedWithCustomError(ERC20, "ERC2612ExpiredSignature");
+	});
+
+	it("Should not be able to use a permit with the wrong signer", async () => {
+		const _owner = userB.address;
+		const spender = userA.address;
+		const value = 1000;
+		const nonce = await ERC20.nonces(_owner);
+		const deadline = (await ethers.provider.getBlock("latest")).timestamp - 1;
+
+		const domain = {
+			name: "Blackhole",
+			version: "1",
+			chainId: (await ethers.provider.getNetwork()).chainId,
+			verifyingContract: ERC20.address,
+		};
+
+		const types = {
+			Permit: [
+				{ name: "owner", type: "address" },
+				{ name: "spender", type: "address" },
+				{ name: "value", type: "uint256" },
+				{ name: "nonce", type: "uint256" },
+				{ name: "deadline", type: "uint256" },
+			],
+		};
+
+		const permit = {
+			owner: _owner,
+			spender: spender,
+			value: value,
+			nonce: nonce.sub(1),
+			deadline: deadline,
+		};
+
+		const signature = await userA._signTypedData(domain, types, permit);
+		const sig = ethers.utils.splitSignature(signature);
+
+		await expect(
+			ERC20.permit(_owner, spender, value, deadline, sig.v, sig.r, sig.s)
+		).to.be.revertedWithCustomError(ERC20, "ERC2612ExpiredSignature");
+	});
 });
